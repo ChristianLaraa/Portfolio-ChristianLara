e3ef73df6892b148be9182de6675d8730b0472a3
Christian Lara <christian.lara.091204@gmail.com>
2025-04-11 00:17:56 -0600

    add full

---
+++ src/assets/data.js
@@ -0,0 +1,143 @@
+export const socials = [{ logo: 'bx bxl-linkedin-square' }, { logo: 'bx bxl-github' }];
+
+export const info = [{ text: '¿Quien soy?' }, { text: 'Proyectos'}, { text: 'Habilidades' }, { text: 'Contacto' }];
+export const technologies = [
+    { text: 'JavaScript', logo: 'bx bxl-javascript' },
+    { text: 'Python', logo: 'bx bxl-python' },
+    { text: 'Java', logo: 'bx bxl-java'},
+    { text: 'Php', logo: 'bx bxl-php'}
+];
+
+export const data = [
+    { text: 'Excel', logo: 'bx bxs-chat' },
+    { text: 'Leadership', logo: 'bx bxs-group' },
+    { text: 'Creativity', logo: 'bx bxs-bulb' },
+];
+
+export const tools = [
+    { text: 'VS Code', logo: 'bx bxl-visual-studio' },
+    { text: 'Git', logo: 'bx bxl-git' },
+    { text: 'Figma', logo: 'bx bxl-figma' },
+];
+
+export const frameworks = [
+    { text: 'Next.js', logo: 'bx bxl-nextjs' },
+    { text: 'Tailwind CSS', logo: 'bx bxl-tailwind-css' },
+    { text: 'Node.js', logo: 'bx bxl-nodejs' },
+];
+
+export const projects = [
+   {
+      title: 'Desidia mx',
+      description:
+         'Desidia mx, es la creación y desarrollo de una plataforma que promueve la música en forma de reseñas, recomendaciones, noticias, eventos, entre otros. La plataforma esta diseñada para que sea intuitiva, visualmente atractiva y funcional esta desarrollado con HTML, CSS y JavaScript. El objetivo de la plataforma es promover la música en México y el mundo, así como fomentar la cultura musical entre los usuarios.',
+      image: 'image-desidiamx',
+      color: '#f7030c',
+      repo: 'https://github.com/ChristianLaraa/Desidia',
+      demo: 'https://christianlaraa.github.io/Desidia/MenuDesidia.html',
+   },
+   {
+      title: 'Analisis de moda emergente en México',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-ecommerce',
+      color: '#045fb7',
+      repo: 'https://github.com/ChristianLaraa/Data/tree/main/Proyecto2',
+      demo: '#',
+   },
+   {
+      title: 'Analisis de Consumo (PT) - Mercado Pago',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-facebook-clone',
+      color: '#146866',
+      repo: 'https://github.com/ChristianLaraa/Data/tree/main/Proyecto3',
+      demo: '#',
+   },
+   {
+      title: 'Landing Page - CNU, UNAM',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-cnu',
+      color: '#17193f',
+      repo: 'https://github.com/ChristianLaraa/webPage_CNU',
+      demo: 'https://centro-de-negocios-universitarios.github.io/webPage_CNU/',
+   },
+   {
+      title: 'Login Web y CRUD de usuarios',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-web1',
+      color: '#f3c201',
+      repo: 'https://github.com/ChristianLaraa/ProyectoWeb1',
+      demo: 'https://christianlaraa.github.io/ProyectoWeb1/',
+   },
+   {
+      title: 'Vinyl Beat - Tienda en Linea',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-slider',
+      color: '#bbbbbb',
+   },
+   {
+      title: 'Cifrado de Textos',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-cifrado',
+      color: '#49d53c',
+      repo: 'https://github.com/ChristianLaraa/DisenoAnalisisAlgoritmos/tree/main/proyecto',
+      demo: '',
+   },
+   {
+      title: 'IA, [Educación financiera y apoyo a transacciones] - Hackathon Banorte 2024',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-hackathon',
+      color: '#f7030c',
+      repo: 'https://github.com/ChristianLaraa/hackaton1',
+      demo: '',
+   },
+   {
+      title: 'Diseño, desarrollo y gestión de Base de Datos Relacional',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-bd',
+      color: '#f7030c',
+      repo: 'https://github.com/ChristianLaraa/ProyectFinalBD1',
+      demo: ''
+   },
+   {
+      title: 'POO - Estaciones del Metro',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-poo',
+      color: '#f7030c',
+      repo: 'https://github.com/ChristianLaraa/ProyectoFinalEstacionesMetro_LMCG',
+      demo: ''
+   },
+   {
+      title: 'BrainWave',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-brainwave',
+      color: '#f7030c',
+   },
+   {
+      title: 'Calculadora de resitencias y capacitores',
+      description:
+         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+      image: 'image-calc',
+      color: '#f7030c',
+      repo: 'https://github.com/ChristianLaraa/PF_EyM',
+      demo: ''
+   },
+];
+
+export const buttons = [
+   {
+      github: 'bx bxl-github',
+      demo: 'bx bx-desktop',
+      back: 'bx bxs-left-arrow-circle',
+      hrefgithub: 'https://github.com/ChristianLaraa'
+   },
+];
