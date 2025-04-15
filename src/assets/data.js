8cf71e88dfed2fc9a4f246fd16e815e5da5bcd5b
Christian Lara <christian.lara.091204@gmail.com>
2025-04-15 00:21:30 -0600

    add posible final

--- src/assets/data.js
+++ src/assets/data.js
@@ -39,95 +39,96 @@ export const projects = [
    {
       title: 'Analisis de moda emergente en México',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Proyecto de análisis de datos enfocado en identificar patrones y tendencias dentro de la industria de moda emergente en México. A través de herramientas de visualización y procesamiento de datos, se exploran aspectos como el crecimiento de marcas independientes, preferencias de consumo y cambios en la estética contemporánea. El proyecto permite observar el comportamiento del consumidor y su impacto cultural, apoyando la toma de decisiones estratégicas para emprendedores y diseñadores. Se utilizan herramientas de analisis, extraccion y visualizacion de datos, WebScrapping en tiendas en linea com Python, SQL para almacenamiento y consulta de datos, Python y Jupyter para Analisis y Tableau para visualización',
       image: 'image-ecommerce',
-      color: '#045fb7',
+      color: '#ff8023',
       repo: 'https://github.com/ChristianLaraa/Data/tree/main/Proyecto2',
       demo: '#',
    },
    {
       title: 'Analisis de Consumo (PT) - Mercado Pago',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Este proyecto explora el comportamiento de consumo de los usuarios de Mercado Pago a partir del análisis de datos obtenidos mediante Python y herramientas estadísticas. Se identifican categorías de gasto, frecuencia de transacciones y patrones recurrentes que permiten comprender mejor la relación entre tecnología financiera y hábitos de consumo. Es una aproximación útil para empresas que buscan optimizar estrategias de marketing y fidelización. Se utiliza Excel, SQL, Python y PowerBi para el análisis y visualización de datos.',
       image: 'image-facebook-clone',
-      color: '#146866',
+      color: '#1d80ff',
       repo: 'https://github.com/ChristianLaraa/Data/tree/main/Proyecto3',
       demo: '#',
    },
    {
       title: 'Landing Page - CNU, UNAM',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Diseño y desarrollo de la landing page oficial del Centro de Negocios Universitarios (CNU), una organización estudiantil de la UNAM. El sitio transmite profesionalismo, dinamismo y comunidad, alineado con la identidad visual del CNU. Fue desarrollado con HTML, CSS y JavaScript, optimizado para ser responsivo y fácilmente mantenible. La página cumple funciones de presentación institucional, convocatoria a eventos y contacto. Prototipado en HTML, CSS y JS, se realizan mejoras continuas en pro de la organización implementando tecnologias como Astro, Tailwind y React',
       image: 'image-cnu',
-      color: '#17193f',
+      color: '#ff2a23',
       repo: 'https://github.com/ChristianLaraa/webPage_CNU',
       demo: 'https://centro-de-negocios-universitarios.github.io/webPage_CNU/',
    },
    {
       title: 'Login Web y CRUD de usuarios',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Sistema completo de autenticación y gestión de usuarios que incluye funciones de login, registro, edición y eliminación de cuentas. Desarrollado con PHP, HTML y CSS, este proyecto simula el flujo básico de un sistema real de usuarios, ideal para proyectos web que requieren control de acceso. Implementa validaciones, alertas y almacenamiento de datos de forma estructurada, destacando por su claridad y funcionalidad - Proyecto final de la materia de Programación Web 1.',
       image: 'image-web1',
-      color: '#f3c201',
+      color: '#ebff23',
       repo: 'https://github.com/ChristianLaraa/ProyectoWeb1',
       demo: 'https://christianlaraa.github.io/ProyectoWeb1/',
    },
    {
       title: 'Vinyl Beat - Tienda en Linea',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Diseño de una tienda en línea ficticia especializada en vinilos, centrada en la experiencia del usuario y la presentación visual atractiva. La interfaz simula una tienda real con productos, sliders, y secciones promocionales. Está enfocada en prácticas de diseño responsivo y se convierte en una base ideal para futuras implementaciones con backend o pasarelas de pago. Desarrollada con React, CSS y TypeScript.',
       image: 'image-slider',
       color: '#bbbbbb',
    },
    {
       title: 'Cifrado de Textos',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Aplicación desarrollada para cifrar y descifrar textos utilizando algoritmos de sustitución y desplazamiento. Este proyecto demuestra habilidades en estructuras de datos, diseño de algoritmos y procesamiento de cadenas, todo implementado en JavaScript. Ideal como introducción a la criptografía clásica y seguridad básica de la información.',
       image: 'image-cifrado',
-      color: '#49d53c',
+      color: '#23c6ff',
       repo: 'https://github.com/ChristianLaraa/DisenoAnalisisAlgoritmos/tree/main/proyecto',
       demo: '',
    },
    {
-      title: 'IA, [Educación financiera y apoyo a transacciones] - Hackathon Banorte 2024',
+      title: 'IA - Hackathon Banorte 2024',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Desarrollo de un agente inteligente diseñado para brindar asesoría financiera personalizada y facilitar transacciones seguras entre jóvenes de 18 a 30 años. El proyecto fue parte del Hackathon Banorte 2024 y combina inteligencia artificial, diseño UX/UI, y simulación de operaciones bancarias en un entorno moderno y educativo. La propuesta se enfoca en generar confianza, accesibilidad e impacto social. El desarrollo se llevo a cabo con las tecnonologias de Figma, React, Tauri, Python, JavaScript, HTML, Rust, Docker, Shell y modelos de IA como ChatGPT, Bard, Ollama',
       image: 'image-hackathon',
       color: '#f7030c',
       repo: 'https://github.com/ChristianLaraa/hackaton1',
       demo: '',
    },
    {
-      title: 'Diseño, desarrollo y gestión de Base de Datos Relacional',
+      title: 'Diseño y gestión de Base de Datos Relacional',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Proyecto completo de base de datos que abarca el modelado entidad-relación, implementación con SQL y consultas optimizadas. El sistema permite gestionar información de forma estructurada, aplicando claves foráneas, normalización y procedimientos almacenados. Ideal para empresas o proyectos que requieren una base de datos sólida y escalable.',
       image: 'image-bd',
-      color: '#f7030c',
+      color: '#c323ff',
       repo: 'https://github.com/ChristianLaraa/ProyectFinalBD1',
       demo: ''
    },
    {
       title: 'POO - Estaciones del Metro',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Proyecto académico que simula un sistema de estaciones del metro utilizando Programación Orientada a Objetos en Java. Incluye clases para estaciones, rutas y conexiones entre líneas. El sistema permite navegar entre estaciones y calcular rutas, enfocándose en el diseño modular y reutilización de código. Es un claro ejemplo de cómo aplicar POO a problemas reales de transporte urbano.',
       image: 'image-poo',
-      color: '#f7030c',
+      color: '#ff73d9',
       repo: 'https://github.com/ChristianLaraa/ProyectoFinalEstacionesMetro_LMCG',
       demo: ''
    },
    {
       title: 'BrainWave',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'BrainWave es una iniciativa conceptual de plataforma interactiva que fusiona creatividad, tecnología y bienestar. Aunque aún en desarrollo, la visión es conectar ideas, emociones y conocimientos a través de experiencias digitales enriquecidas, inteligencia artificial y diseño intuitivo. Este proyecto representa una apuesta por la innovación y el desarrollo humano desde lo digital. La plataforma busca ser un espacion en donde estudiantes puedan aprender nuevas habilidades y reforzar lo visto en sus clases, se realiza prototipado y diseño de la plataforma en Figma, tambien se crea el branding y la identidad de la marca.',
       image: 'image-brainwave',
-      color: '#f7030c',
+      color: '#fff474',
+      demo: ''
    },
    {
       title: 'Calculadora de resitencias y capacitores',
       description:
-         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.',
+         'Herramienta digital desarrollada para calcular automáticamente los valores de resistencias y capacitores en circuitos eléctricos. Esta aplicación facilita el trabajo de estudiantes y profesionales de electrónica al simplificar los cálculos y mostrar resultados precisos de acuerdo con códigos de colores y fórmulas técnicas. Se enfoca en funcionalidad, precisión y accesibilidad. Desarrollado completamente en Python con ayuda de librerias para el desarrollo de interfaces graficas como Tkinter y PySimpleGUI, se implementa un diseño responsivo y amigable para el usuario.',
       image: 'image-calc',
-      color: '#f7030c',
+      color: '#74ffc2',
       repo: 'https://github.com/ChristianLaraa/PF_EyM',
       demo: ''
    },
